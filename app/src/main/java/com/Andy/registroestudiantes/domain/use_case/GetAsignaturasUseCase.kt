9c33bba079458c04796465bf6333bb00efa24269
Andy <andyjavierrd@gmail.com>
2026-01-23 23:33:35 -0400

    Agregar GetAsignaturasUseCase en capa de dominio

---
+++ app/src/main/java/com/Andy/registroestudiantes/domain/use_case/GetAsignaturasUseCase.kt
@@ -0,0 +1,12 @@
+package com.Andy.registroestudiantes.domain.use_case
+
+import com.Andy.registroestudiantes.domain.model.Asignatura
+import com.Andy.registroestudiantes.domain.repository.AsignaturaRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetAsignaturasUseCase @Inject constructor(
+    private val repository: AsignaturaRepository
+) {
+    operator fun invoke(): Flow<List<Asignatura>> = repository.getAll()
+}
