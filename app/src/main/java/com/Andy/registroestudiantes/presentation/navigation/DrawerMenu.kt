928b2d88345e542df256f05acdca44c2c5aa2e03
Andy <andyjavierrd@gmail.com>
2026-01-30 22:55:44 -0400

    Agregue opción de Penalidades al Drawer

--- app/src/main/java/com/Andy/registroestudiantes/presentation/navigation/DrawerMenu.kt
+++ app/src/main/java/com/Andy/registroestudiantes/presentation/navigation/DrawerMenu.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
